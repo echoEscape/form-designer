dec9594c4c71a7fa45ee05f31bf0a76537991a33
echoEscape <thenumberiszero@gmail.com>
2024-12-10 23:49:07 +0100

    Setup basic layout grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { OverviewComponent } from './overview/overview/overview.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent, OverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/editor/viewport/viewport.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewport',
+  imports: [],
+  templateUrl: './viewport.component.html',
+  styleUrl: './viewport.component.css'
+})
+export class ViewportComponent {
+
+}
